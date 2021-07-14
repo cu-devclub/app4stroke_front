2e29e4fca251adfbebfc461792f9ff6794dc45a3
palmmy2542 <palm_my2542@hotmail.com>
2021-07-15 02:40:24 +0700

    Test

--- src/Pages/nong1/Containers/TitleSection.tsx
+++ src/Pages/nong1/Containers/TitleSection.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Title from "../Components/Title";
 
 const TitleSection: React.FC = () => {
-  return <Title />;
+  return <Title name="nong-1" />;
 };
 
 export default TitleSection;
