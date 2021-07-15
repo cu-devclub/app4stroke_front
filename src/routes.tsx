a485463d77feb6d2a7137a384aff55cf91caff69
Wannakorn Cherngchaosil <bobbibrown.1502@gmail.com>
2021-07-15 22:23:59 +0700

    Add Fern page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import SignInPage from "./Pages/signin/index";
+import Fern from './Pages/fern/index';
 const Routes: React.FC = () => {
   return (
     <Switch>
+      <Route path="/fern">
+        <Fern/>
+      </Route>
       <Route path="/">
         <SignInPage />
       </Route>
