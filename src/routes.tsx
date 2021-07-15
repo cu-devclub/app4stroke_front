784c25cd668b1e231853f26b65ceb76284feab14
palmmy2542 <palm_my2542@hotmail.com>
2021-07-15 22:23:57 +0700

    Add Palm page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import SignInPage from "./Pages/signin/index";
+import Palm from "./Pages/palm/index";
+
 const Routes: React.FC = () => {
   return (
     <Switch>
+      <Route path="/palm">
+        <Palm />
+      </Route>
       <Route path="/">
         <SignInPage />
       </Route>
