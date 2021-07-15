79ca52c63c0b986b467b2a7e5fff61446ff5f66d
Wannakorn Cherngchaosil <bobbibrown.1502@gmail.com>
2021-07-15 22:42:39 +0700

    Change name to FernFern

--- src/Pages/fern/Containers/TitleContainer.tsx
+++ src/Pages/fern/Containers/TitleContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Title from "../Components/Title";
 
 const TitleContainer: React.FC = () => {
-  return <Title name={"Fern"} />;
+  return <Title name={"FernFern"} />;
 };
 
 export default TitleContainer;
