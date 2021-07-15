22df3aa2c24a4857832aba0f37a8619cb7073877
Wannakorn Cherngchaosil <bobbibrown.1502@gmail.com>
2021-07-15 22:22:55 +0700

    Create Title component

---
+++ src/Pages/fern/Components/Title.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+  name?: string;
+}
+
+const Title = (props: Props) => {
+  const { name } = props;
+  return <h1>{name}</h1>;
+};
+export default Title;
