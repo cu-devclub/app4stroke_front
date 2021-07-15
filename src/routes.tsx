947090e00b508bfb072ddec5fca17fd502701128
Bunyanut Suwankosit <amsteel23@gmail.com>
2021-07-15 22:26:19 +0700

    Add PageSteel

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import SignInPage from "./Pages/signin/index";
+import Steel from "./Pages/steel/index";
 const Routes: React.FC = () => {
   return (
     <Switch>
+      <Route path="/name">
+        <Steel />
+      </Route>
       <Route path="/">
         <SignInPage />
       </Route>
