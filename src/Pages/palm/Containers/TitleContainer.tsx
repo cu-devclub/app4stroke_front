06c58072ec46d92474d7d444b610fc2577fcb095
palmmy2542 <palm_my2542@hotmail.com>
2021-07-15 22:23:39 +0700

    Create TitleContainer

---
+++ src/Pages/palm/Containers/TitleContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Title from "../Components/Title";
+
+const TitleContainer: React.FC = () => {
+  return <Title name={"Palm"} />;
+};
+
+export default TitleContainer;
