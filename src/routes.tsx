1760c5ea4154c40175af07ad647d416d3b72e4e3
pornthip-n <pawnthip@gmail.com>
2021-07-15 22:24:31 +0700

    add nan page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import SignInPage from "./Pages/signin/index";
+import Nan from "./Pages/nan/index";
 const Routes: React.FC = () => {
   return (
     <Switch>
+      <Route path="/nan">
+        <Nan />
+      </Route>
       <Route path="/">
         <SignInPage />
       </Route>
