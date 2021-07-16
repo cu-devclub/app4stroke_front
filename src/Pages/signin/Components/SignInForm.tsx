9e5c4aa62d51ec274fd330a6fb31f8094bc7c6ce
Wannakorn Cherngchaosil <bobbibrown.1502@gmail.com>
2021-07-16 16:38:16 +0700

    Add login logo

--- src/Pages/signin/Components/SignInForm.tsx
+++ src/Pages/signin/Components/SignInForm.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import background from "../../../assets/StrokeBackground.png";
+import Background from "../../../assets/StrokeBackground.png";
+import LoginLogo from "../../../assets/LoginLogo.png";
 
 //background
 export const StyledContainer = styled.div`
@@ -10,15 +11,39 @@ export const StyledContainer = styled.div`
   justify-content: center;
   align-items: center;
   background: linear-gradient(0deg, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)),
-    url(${background});
+    url(${Background});
   background-size: cover;
   background-attachment: fixed;
 `;
-
+//loginbox
+export const StyledFormArea = styled.div`
+  position: absolute;
+  width: 500px;
+  height: 400px;
+  left: 500px;
+  top: 134px;
+  background: rgba(255, 255, 255, 0.6);
+  backdrop-filter: blur(4px);
+  border-radius: 40px;
+`;
+//loginlogo
+export const Avatar = styled.div`
+  width: 250px;
+  height: 100px;
+  background-image: url(${LoginLogo});
+  background-size: cover;
+  background-position: center;
+  margin: auto;
+  margin-top: 30px;
+`;
 const SignInForm: React.FC = () => {
   return (
     <>
-      <StyledContainer></StyledContainer>
+      <StyledContainer>
+        <StyledFormArea>
+          <Avatar/>
+        </StyledFormArea>
+      </StyledContainer>
     </>
   );
 };
