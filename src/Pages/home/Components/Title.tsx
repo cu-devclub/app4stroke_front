3c9e26ee70773eca06e7fba19e4da496c48e5c70
pornthip-n <pawnthip@gmail.com>
2021-07-17 21:56:43 +0700

    change name from Welcome to Title

--- src/Pages/home/Components/Title.tsx
+++ src/Pages/home/Components/Title.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import brainImg from "../../../assets/brain_welcome.png";
+import AddIcon from "../../../assets/add.png";
+
 //todo Style
 const useStyle = makeStyles(() =>
   createStyles({
     root: {
       flexGrow: 1,
     },
+
+    //! Box
     boxWelcome: {
       width: "616px", //decrease 15%
       height: "147px",
@@ -22,26 +28,51 @@ const useStyle = makeStyles(() =>
       width: "185px",
       height: "147px",
       marginTop: "48px",
-      marginLeft: "32px",
+      marginLeft: "24px",
       background: "#FFFFFF",
       boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
       borderRadius: " 20px",
     },
+    //! Text
     textWelcome: {
       marginLeft: "23px",
       color: "white",
     },
-    textTotalPatients: {
+    textTotal: {
       marginLeft: "23px",
+      marginTop: "27px",
     },
+    textTotalPatients: {
+      color: "#979797",
+      fontWeight: "normal",
+    },
+    textRegister: {
+      textTransform: "none",
+      color: "white",
+      fontWeight: "lighter",
+    },
+    //! Button
+    buttonRegister: {
+      backgroundColor: "#EF5DA8",
+      width: "307px",
+      height: "144px",
+      marginLeft: "47px",
+      marginTop: "51px",
+      borderRadius: "24px",
+    },
+    //! Icon
+    // addIcon: {
+    //   marginTop: "10px",
+    // },
   })
 );
 
-const Welcome: React.FC = () => {
+const Title: React.FC = () => {
   const classes = useStyle();
   return (
     <div className={classes.root}>
       <Box display="flex" justifyContent="start">
+        {/* Welcom Doctor */}
         <Box border={0} className={classes.boxWelcome}>
           <div className={classes.textWelcome}>
             <p>Welcome</p>
@@ -50,15 +81,28 @@ const Welcome: React.FC = () => {
             <img src={brainImg} alt="icon" />
           </div>
         </Box>
+
+        {/* Total Patients */}
         <Box border={0} className={classes.boxTotalPatients}>
-          <div className={classes.textTotalPatients}>
-            <h6>Total Patients</h6>
+          <div className={classes.textTotal}>
+            <h5 className={classes.textTotalPatients}>Total Patients</h5>
             <h1>20</h1>
           </div>
         </Box>
+        {/* Register New Patients */}
+        <Button variant="contained" className={classes.buttonRegister}>
+          <div>
+            <div>
+              <img src={AddIcon} alt="icon" />
+            </div>
+            <div className={classes.textRegister}>
+              <Typography>Register New Patients</Typography>
+            </div>
+          </div>
+        </Button>
       </Box>
     </div>
   );
 };
 
-export default Welcome;
+export default Title;
--- src/Pages/home/Containers/HomeContainer.tsx
+++ src/Pages/home/Containers/HomeContainer.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "../Components/Header";
 import Box from "@material-ui/core/Box";
-import Welcome from "../Components/Welcome";
+import Title from "../Components/Title";
 
 const HomeContainer: React.FC = () => {
   return (
     <Box>
       <Header />
-      <Welcome />
+      <Title />
     </Box>
   );
 };
