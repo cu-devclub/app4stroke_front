8ac56eb2f07ad9856c0360ecd8558f781d87b2b3
pornthip-n <pawnthip@gmail.com>
2021-07-17 17:12:55 +0700

    welcome and total patients

--- src/Pages/home/Components/Welcome.tsx
+++ src/Pages/home/Components/Welcome.tsx
@@ -8,7 +8,7 @@ const useStyle = makeStyles(() =>
     root: {
       flexGrow: 1,
     },
-    box: {
+    boxWelcome: {
       width: "616px", //decrease 15%
       height: "147px",
       marginTop: "48px",
@@ -18,9 +18,21 @@ const useStyle = makeStyles(() =>
         "linear-gradient(94.59deg, #5028C6 9.91%, #FD85FF 81.34%, #F40000 111.1%)",
       boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
     },
-    text: {
+    boxTotalPatients: {
+      width: "185px",
+      height: "147px",
+      marginTop: "48px",
+      marginLeft: "32px",
+      background: "#FFFFFF",
+      boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+      borderRadius: " 20px",
+    },
+    textWelcome: {
       marginLeft: "23px",
       color: "white",
+    },
+    textTotalPatients: {
+      marginLeft: "23px",
     },
   })
 );
@@ -30,12 +42,18 @@ const Welcome: React.FC = () => {
   return (
     <div className={classes.root}>
       <Box display="flex" justifyContent="start">
-        <Box border={0} className={classes.box}>
-          <div className={classes.text}>
+        <Box border={0} className={classes.boxWelcome}>
+          <div className={classes.textWelcome}>
             <p>Welcome</p>
             <h4>Dr. Sawasdee</h4>
             <h2>Chulalongkorn Stroke Center</h2>
             <img src={brainImg} alt="icon" />
+          </div>
+        </Box>
+        <Box border={0} className={classes.boxTotalPatients}>
+          <div className={classes.textTotalPatients}>
+            <h6>Total Patients</h6>
+            <h1>20</h1>
           </div>
         </Box>
       </Box>
