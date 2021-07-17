d221fffc50ba3f8ce4d5bcb46a97f1857a07c823
Bunyanut Suwankosit <amsteel23@gmail.com>
2021-07-17 18:53:40 +0700

    Add path-/home

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import SignInPage from "./Pages/signin/index";
+import HomeContainer from "./Pages/home";
 
 const Routes: React.FC = () => {
   return (
     <Switch>
+      <Route path="/home">
+        <HomeContainer />
+      </Route>
       <Route path="/">
         <SignInPage />
       </Route>
