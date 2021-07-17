d184522aad57edc055e7851acd13e916faa95a50
Bunyanut Suwankosit <amsteel23@gmail.com>
2021-07-17 18:53:02 +0700

    Create SearchTable

---
+++ src/Pages/home/Components/SearchTable.tsx
@@ -0,0 +1,76 @@
+import {
+  makeStyles,
+  Typography,
+  Toolbar,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import React from "react";
+
+const useStyles = makeStyles((theme) => ({
+  pageTitle: {
+    paddingLeft: theme.spacing(5),
+    padding: theme.spacing(3),
+    paddingBottom: theme.spacing(0),
+    "& .MuiTypography-subtitle2": {
+      color: "#979797",
+      marginTop: "12%",
+      fontSize: "16px",
+    },
+    "& .MuiTypography-h6": {
+      fontSize: "24px",
+    },
+  },
+  search: {
+    marginLeft: "20%",
+    marginTop: "2.5%",
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "200px",
+    },
+  },
+  buttonSearch: {
+    backgroundColor: "#EF5DA8",
+    color: "#ffffff",
+    textTransform: "none",
+    borderRadius: "20px",
+    fontSize: "18px",
+  },
+}));
+
+const SearchTables = (props: any) => {
+  const classes = useStyles();
+
+  const { title, subTitle } = props;
+
+  return (
+    <>
+      <Toolbar>
+        <div className={classes.pageTitle}>
+          <Typography variant="h6" component="div">
+            {title}
+          </Typography>
+          <Typography variant="subtitle2" component="div">
+            {subTitle}
+          </Typography>
+        </div>
+        <Toolbar className={classes.search}>
+          <TextField id="standard-basic" label="Patient ID"></TextField>
+          <TextField id="standard-basic" label="Name"></TextField>
+          <TextField
+            id="date"
+            label="Date"
+            type="date"
+            defaultValue=""
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <Button className={classes.buttonSearch}> Search </Button>
+        </Toolbar>
+      </Toolbar>
+    </>
+  );
+};
+
+export default SearchTables;
--- src/Pages/home/Containers/HomeContainers.tsx
+++ src/Pages/home/Containers/HomeContainers.tsx
@@ -6,6 +6,7 @@ import {
   Button,
 } from "@material-ui/core";
 import React from "react";
+import SearchTables from "../Components/SearchTable";
 import Tables, { createData } from "../Components/Tables";
 
 const useStyles = makeStyles((theme) => ({
@@ -43,10 +44,15 @@ const headCells = [
 
 const rows = [
   createData("1150", "28/02/2564", "12345678", "Somachi Tang", 23, 0.23),
-  createData("1134", "19/02/2564", "12567890", "Evan", 31, 0.55),
-  createData("1119", "06/02/2564", "10968899", "Nattasuk Chaithana", 38, 0.12),
-  createData("1118", "06/02/2564", "13456788", "Srisawut", 42, 0.23),
-  createData("1107", "04/02/2564", "24567890", "Meethana", 26, 13),
+  createData("1134", "19/02/2564", "12435637", "Evan Jefferson", 31, 0.55),
+  createData("1119", "06/02/2564", "10003484", "Nattasuk Chaithana", 38, 0.12),
+  createData("1118", "06/02/2564", "10475995", "Srisawut Somkierd", 42, 0.23),
+  createData("1107", "04/02/2564", "20384857", "Meethana Julsiri", 26, 13),
+  createData("1092", "25/01/2564", "20038480", "Sanphop Klinpipat", 63, 18),
+  createData("1088", "25/01/2564", "10004832", "Malini Smith", 49, 22),
+  createData("1056", "16/01/2564", "10003884", "Evan Jeffer", 68, 27),
+  createData("1045", "15/01/2564", "17399400", "Leelerd Somjai", 50, 19),
+  createData("1001", "12/01/2564", "19293948", "Srisaket Charoensri", 37, 25),
 ];
 
 const { TblContainer, TblHead, TblCenter } = Tables(headCells);
@@ -55,26 +61,29 @@ const HomeContainer: React.FC = () => {
   const classes = useStyles();
 
   return (
-    <Paper className={classes.pageContent}>
-      <TblContainer>
-        <TblHead />
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.testID} className={classes.root}>
-              <TblCenter>{row.testID}</TblCenter>
-              <TblCenter>{row.date}</TblCenter>
-              <TblCenter>{row.patientID}</TblCenter>
-              <TblCenter>{row.name}</TblCenter>
-              <TblCenter>{row.age}</TblCenter>
-              <TblCenter>{row.cardioembolicProbability}</TblCenter>
-              <TblCenter>
-                <Button className={classes.buttonView}>View</Button>
-              </TblCenter>
-            </TableRow>
-          ))}
-        </TableBody>
-      </TblContainer>
-    </Paper>
+    <>
+      <SearchTables title="Patient Tests " subTitle="Total 63 records " />
+      <Paper className={classes.pageContent}>
+        <TblContainer>
+          <TblHead />
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.testID} className={classes.root}>
+                <TblCenter>{row.testID}</TblCenter>
+                <TblCenter>{row.date}</TblCenter>
+                <TblCenter>{row.patientID}</TblCenter>
+                <TblCenter>{row.name}</TblCenter>
+                <TblCenter>{row.age}</TblCenter>
+                <TblCenter>{row.cardioembolicProbability}</TblCenter>
+                <TblCenter>
+                  <Button className={classes.buttonView}>View</Button>
+                </TblCenter>
+              </TableRow>
+            ))}
+          </TableBody>
+        </TblContainer>
+      </Paper>
+    </>
   );
 };
 export default HomeContainer;
