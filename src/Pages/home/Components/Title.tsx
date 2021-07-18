c0f8374081937defdff9ab0682233d5411a6b48f
pornthip-n <pawnthip@gmail.com>
2021-07-18 15:56:05 +0700

    edit Title

--- src/Pages/home/Components/Title.tsx
+++ src/Pages/home/Components/Title.tsx
@@ -3,31 +3,31 @@ import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
-import brainImg from "../../../assets/brain_welcome.png";
 import AddIcon from "../../../assets/add.png";
 
 //todo Style
 const useStyle = makeStyles(() =>
   createStyles({
     root: {
-      flexGrow: 1,
+      width: "100%",
+      marginTop: "16px",
+      padding: "32px",
     },
 
     //! Box
     boxWelcome: {
-      width: "616px", //decrease 15%
-      height: "147px",
-      marginTop: "48px",
-      marginLeft: "32px",
+      width: "45%",
+      height: "93px",
+      padding: "27px",
       borderRadius: "20px",
       background:
         "linear-gradient(94.59deg, #5028C6 9.91%, #FD85FF 81.34%, #F40000 111.1%)",
       boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
     },
     boxTotalPatients: {
-      width: "185px",
-      height: "147px",
-      marginTop: "48px",
+      width: "10%",
+      height: "93px",
+      padding: "27px",
       marginLeft: "24px",
       background: "#FFFFFF",
       boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
@@ -35,12 +35,8 @@ const useStyle = makeStyles(() =>
     },
     //! Text
     textWelcome: {
-      marginLeft: "23px",
       color: "white",
-    },
-    textTotal: {
-      marginLeft: "23px",
-      marginTop: "27px",
+      width: "50%",
     },
     textTotalPatients: {
       color: "#979797",
@@ -54,54 +50,53 @@ const useStyle = makeStyles(() =>
     //! Button
     buttonRegister: {
       backgroundColor: "#EF5DA8",
-      width: "307px",
+      width: "25%",
       height: "144px",
-      marginLeft: "47px",
-      marginTop: "51px",
       borderRadius: "24px",
+      marginLeft: "47px",
+      marginRight: "32px",
     },
-    //! Icon
-    // addIcon: {
-    //   marginTop: "10px",
-    // },
   })
 );
 
 const Title: React.FC = () => {
   const classes = useStyle();
   return (
-    <div className={classes.root}>
-      <Box display="flex" justifyContent="start">
-        {/* Welcom Doctor */}
+    <Box className={classes.root}>
+      <Box display="flex">
+        {/* Welcome Doctor */}
         <Box border={0} className={classes.boxWelcome}>
-          <div className={classes.textWelcome}>
-            <p>Welcome</p>
-            <h4>Dr. Sawasdee</h4>
-            <h2>Chulalongkorn Stroke Center</h2>
-            <img src={brainImg} alt="icon" />
-          </div>
+          <Box className={classes.textWelcome} display="inline">
+            <Typography variant="body2">Welcome</Typography>
+            <Typography variant="h6">Dr. Sawasdee</Typography>
+            <Typography variant="h5">Chulalongkorn Stroke Center</Typography>
+          </Box>
         </Box>
-
         {/* Total Patients */}
         <Box border={0} className={classes.boxTotalPatients}>
-          <div className={classes.textTotal}>
-            <h5 className={classes.textTotalPatients}>Total Patients</h5>
-            <h1>20</h1>
-          </div>
+          <Box>
+            <Typography
+              variant="subtitle1"
+              className={classes.textTotalPatients}
+            >
+              Total Patients
+            </Typography>
+            <Typography variant="h2">20</Typography>
+          </Box>
         </Box>
         {/* Register New Patients */}
         <Button variant="contained" className={classes.buttonRegister}>
-          <div>
-            <div>
+          <Box display="flex" flexDirection="column">
+            <Box>
               <img src={AddIcon} alt="icon" />
-            </div>
-            <div className={classes.textRegister}>
-              <Typography>Register New Patients</Typography>
-            </div>
-          </div>
+            </Box>
+            <Box className={classes.textRegister}>
+              <Typography variant="subtitle1">Register New Patients</Typography>
+            </Box>
+          </Box>
         </Button>
       </Box>
-    </div>
+    </Box>
   );
 };
 
