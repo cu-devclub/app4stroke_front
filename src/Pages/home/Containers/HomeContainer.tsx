c8e079a235b6a8c68100b40e60e842ced0e9a1b0
pornthip-n <pawnthip@gmail.com>
2021-07-18 15:57:34 +0700

    edit HomeContainer

--- src/Pages/home/Containers/HomeContainer.tsx
+++ src/Pages/home/Containers/HomeContainer.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Header from "../Components/Header";
-import Box from "@material-ui/core/Box";
 import Title from "../Components/Title";
 
 const HomeContainer: React.FC = () => {
   return (
-    <Box>
+    <>
       <Header />
       <Title />
-    </Box>
+    </>
   );
 };
 
