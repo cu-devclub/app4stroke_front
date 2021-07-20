a70fb47da0727665ebf1b0cdade61cc38d893440
pornthip-n <pawnthip@gmail.com>
2021-07-20 16:04:58 +0700

    SectionTitle Patient Information Done

--- src/Pages/Form/Components/SectionTitle.tsx
+++ src/Pages/Form/Components/SectionTitle.tsx
@@ -1,8 +1,42 @@
-import Typography from "@material-ui/core/Typography";
 import React from "react";
+import { Box } from "@material-ui/core";
+import Divider from "@material-ui/core/Divider";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+
+const useStyle = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%",
+    },
+    text: {
+      "&.MuiDivider-root": {
+        "&::before": {
+          borderTop: "none",
+        },
+        "&::after": {
+          borderTop: "thin solid #D3D3D3",
+          marginTop: "10px",
+        },
+      },
+      "& .MuiDivider-wrapper": {
+        fontSize: 36,
+      },
+    },
+  })
+);
 
 const SectionTitle: React.FC = () => {
-  return <Typography>Patient Information</Typography>;
+  const classes = useStyle();
+  return (
+    <Box>
+      <Box className={classes.root}>
+        <Divider className={classes.text} textAlign="left">
+          Patient Information
+        </Divider>
+      </Box>
+    </Box>
+  );
 };
 
 export default SectionTitle;
