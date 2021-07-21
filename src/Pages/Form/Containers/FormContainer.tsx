08daf7b4221ae1b513e3f36d81498d08ffbf471a
pornthip-n <pawnthip@gmail.com>
2021-07-21 16:09:40 +0700

    remove SectionTitle from Container

--- src/Pages/Form/Containers/FormContainer.tsx
+++ src/Pages/Form/Containers/FormContainer.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import SectionTitle from "../Components/SectionTitle";
 
 const FormContainer: React.FC = () => {
-  return <SectionTitle title="Patient Information"></SectionTitle>;
+  return <></>;
 };
 export default FormContainer;
