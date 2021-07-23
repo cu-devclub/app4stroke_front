98010ef1e4dc6619c6fdd69ac9f72bc2b71de1f6
pornthip-n <pawnthip@gmail.com>
2021-07-23 15:46:15 +0700

    remove margin and padding at root

--- src/Pages/Form/Components/PatientInformationSection.tsx
+++ src/Pages/Form/Components/PatientInformationSection.tsx
@@ -21,17 +21,35 @@ const useStyle = makeStyles((theme) =>
   createStyles({
     root: {
       width: "100%",
-      marginLeft: "32px",
-    },
-    picker: {
-      marginTop: "34px",
     },
     textField: {
-      width: "80%",
+      width: "100%",
     },
     textTitle: {
-      marginTop: "40px",
+      marginBottom: "16px",
     },
+    //* box
+    boxPatient: {
+      marginLeft: "32px",
+      marginBottom: "40px",
+    },
+    boxGender: {
+      marginLeft: "32px",
+      marginBottom: "40px",
+    },
+    boxPicker: {
+      marginLeft: "32px",
+      marginBottom: "40px",
+    },
+    boxOnset: {
+      marginLeft: "32px",
+      marginBottom: "40px",
+    },
+    boxUpload: {
+      marginLeft: "32px",
+      marginBottom: "40px",
+    },
+    //* button
     button: {
       "&.Mui-selected": {
         backgroundColor: "#EF5DA8",
@@ -54,8 +72,8 @@ const useStyle = makeStyles((theme) =>
 
 const GenderStyledToggleButtonGroup = withStyles((theme) => ({
   grouped: {
-    margin: theme.spacing(2),
     marginBottom: "8px",
+    marginRight: "24px",
     boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
     width: "150px",
     height: "95px",
@@ -129,124 +147,134 @@ const PatientInformationSection: React.FC = () => {
   return (
     <Box className={classes.root}>
       {/* Patient - Text Field */}
-      <Grid container spacing={6}>
-        <Grid item xs={4}>
-          <TextField label="Enter patient ID" className={classes.textField} />
+      <Box className={classes.boxPatient}>
+        <Grid container spacing={6}>
+          <Grid item xs={3}>
+            <TextField label="Enter patient ID" className={classes.textField} />
+          </Grid>
+          <Grid item xs={3}>
+            <TextField label="Enter age" className={classes.textField} />
+          </Grid>
         </Grid>
-        <Grid item xs={4}>
-          <TextField label="Enter age" className={classes.textField} />
+        <Grid container spacing={6}>
+          <Grid item xs={3}>
+            <TextField label="Enter first name" className={classes.textField} />
+          </Grid>
+          <Grid item xs={3}>
+            <TextField label="Enter last name" className={classes.textField} />
+          </Grid>
         </Grid>
-      </Grid>
-      <Grid container spacing={6}>
-        <Grid item xs={4}>
-          <TextField label="Enter first name" className={classes.textField} />
-        </Grid>
-        <Grid item xs={4}>
-          <TextField label="Enter last name" className={classes.textField} />
-        </Grid>
-      </Grid>
+      </Box>
       {/* Gender - Button */}
-      <Box className={classes.textTitle}>
-        <Typography variant="h4">Gender</Typography>
+      <Box className={classes.boxGender}>
+        <Box className={classes.textTitle}>
+          <Typography variant="h4">Gender</Typography>
+        </Box>
+        <Grid container spacing={2}>
+          {/* MALE */}
+          <Box>
+            <GenderStyledToggleButtonGroup
+              value={gender}
+              exclusive
+              onChange={handleGender}
+            >
+              <ToggleButton value="male" className={classes.button}>
+                <Box display="flex">
+                  <img src={MaleIcon} alt="male icon" />
+                </Box>
+              </ToggleButton>
+            </GenderStyledToggleButtonGroup>
+            <Box textAlign="center">
+              <Typography variant="body1" margin-bottom="64px">
+                Male
+              </Typography>
+            </Box>
+          </Box>
+          {/* FEMALE */}
+          <Box>
+            <GenderStyledToggleButtonGroup
+              value={gender}
+              exclusive
+              onChange={handleGender}
+            >
+              <ToggleButton value="female" className={classes.button}>
+                <Box display="flex">
+                  <img src={FemaleIcon} alt="female icon" />
+                </Box>
+              </ToggleButton>
+            </GenderStyledToggleButtonGroup>
+            <Box textAlign="center">
+              <Typography variant="body1">Female</Typography>
+            </Box>
+          </Box>
+        </Grid>
       </Box>
-      <Grid container spacing={2}>
-        {/* MALE */}
-        <Box>
-          <GenderStyledToggleButtonGroup
-            value={gender}
-            exclusive
-            onChange={handleGender}
-          >
-            <ToggleButton value="male" className={classes.button}>
-              <Box display="flex">
-                <img src={MaleIcon} alt="male icon" />
-              </Box>
-            </ToggleButton>
-          </GenderStyledToggleButtonGroup>
-          <Box textAlign="center">
-            <Typography variant="body1">Male</Typography>
-          </Box>
-        </Box>
-        {/* FEMALE */}
-        <Box>
-          <GenderStyledToggleButtonGroup
-            value={gender}
-            exclusive
-            onChange={handleGender}
-          >
-            <ToggleButton value="female" className={classes.button}>
-              <Box display="flex">
-                <img src={FemaleIcon} alt="female icon" />
-              </Box>
-            </ToggleButton>
-          </GenderStyledToggleButtonGroup>
-          <Box textAlign="center">
-            <Typography variant="body1">Female</Typography>
-          </Box>
-        </Box>
-      </Grid>
       {/* Arrival Time - Date & Time Picker*/}
-      <Box className={classes.textTitle}>
-        <Typography variant="h4">Arrival Time</Typography>
+      <Box className={classes.boxPicker}>
+        <Box className={classes.textTitle}>
+          <Typography variant="h4">Arrival Time</Typography>
+        </Box>
+        <Grid container spacing={7}>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Grid item>
+              <KeyboardDatePicker
+                variant="inline"
+                format="dd/MM/yyyy"
+                label="Arrival Date"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  "aria-label": "change date",
+                }}
+              />
+            </Grid>
+            <Grid item>
+              <KeyboardTimePicker
+                variant="inline"
+                label="Arrival Time"
+                value={selectedTime}
+                onChange={handleTimeChange}
+                KeyboardButtonProps={{
+                  "aria-label": "change time",
+                }}
+              />
+            </Grid>
+          </MuiPickersUtilsProvider>
+        </Grid>
       </Box>
-      <Grid container spacing={7}>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <Grid item>
-            <KeyboardDatePicker
-              variant="inline"
-              format="dd/MM/yyyy"
-              label="Arrival Date"
-              value={selectedDate}
-              onChange={handleDateChange}
-              KeyboardButtonProps={{
-                "aria-label": "change date",
-              }}
-              className={classes.picker}
-            />
-          </Grid>
-          <Grid item>
-            <KeyboardTimePicker
-              variant="inline"
-              label="Arrival Time"
-              value={selectedTime}
-              onChange={handleTimeChange}
-              KeyboardButtonProps={{
-                "aria-label": "change time",
-              }}
-              className={classes.picker}
-            />
-          </Grid>
-        </MuiPickersUtilsProvider>
-      </Grid>
       {/* Onset - Button */}
-      <Box className={classes.textTitle}>
-        <Typography variant="h4">Onset</Typography>
+      <Box className={classes.boxOnset}>
+        <Box className={classes.textTitle}>
+          <Typography variant="h4">Onset</Typography>
+        </Box>
+        <Grid container spacing={3}>
+          <OnsetStyledToggleButtonGroup
+            value={onset}
+            exclusive
+            onChange={handleOnset}
+          >
+            <ToggleButton value="clearOnset" className={classes.button}>
+              <Box>
+                <Typography variant="subtitle1">Clear onset</Typography>
+              </Box>
+            </ToggleButton>
+            <ToggleButton value="unknownOnset" className={classes.button}>
+              <Box>
+                <Typography variant="subtitle1">Unknown onset</Typography>
+              </Box>
+            </ToggleButton>
+          </OnsetStyledToggleButtonGroup>
+        </Grid>
       </Box>
-      <Grid container spacing={3}>
-        <OnsetStyledToggleButtonGroup
-          value={onset}
-          exclusive
-          onChange={handleOnset}
-        >
-          <ToggleButton value="clearOnset" className={classes.button}>
-            <Box display="flex">
-              <Typography variant="subtitle1">Clear onset</Typography>
-            </Box>
-          </ToggleButton>
-          <ToggleButton value="unknownOnset" className={classes.button}>
-            <Box display="flex">
-              <Typography variant="subtitle1">Unknown onset</Typography>
-            </Box>
-          </ToggleButton>
-        </OnsetStyledToggleButtonGroup>
-      </Grid>
       {/* Upload CT Scan - Button*/}
-      <Box className={classes.textTitle}>
-        <Typography variant="h4">Upload CT Scan</Typography>
+      <Box className={classes.boxUpload}>
+        <Box className={classes.textTitle}>
+          <Typography variant="h4">Upload CT Scan</Typography>
+        </Box>
+        <Button variant="contained" className={classes.buttonUpload}>
+          <Typography variant="subtitle1">Upload</Typography>
+        </Button>
       </Box>
-      <Button variant="contained" className={classes.buttonUpload}>
-        <Typography variant="subtitle1">Upload</Typography>
-      </Button>
     </Box>
   );
 };
