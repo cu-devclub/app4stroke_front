599820b273b60e0efc523bcb6f3b2afaf19b4b85
Wannakorn Cherngchaosil <bobbibrown.1502@gmail.com>
2021-07-23 15:22:12 +0700

    Add ChiefComplaint and EKG12Leads section

--- src/Pages/form/Containers/FormContainer.tsx
+++ src/Pages/form/Containers/FormContainer.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import NIHSSSection from "../Components/NIHSSSection";
 import SectionTitle from "../Components/SectionTitle";
+import ChiefComplaintSection from "../Components/ChiefComplaintSection";
+import EKG12LeadsSection from "../Components/EKG12LeadsSection";
 
 const FormContainer: React.FC = () => {
   return (
     <>
+      <SectionTitle title="Chief Complaint" />
+      <ChiefComplaintSection />
+      <SectionTitle title="EKG 12 Leads" />
+      <EKG12LeadsSection />
       <SectionTitle title="NIHSS" />
       <NIHSSSection />
     </>
