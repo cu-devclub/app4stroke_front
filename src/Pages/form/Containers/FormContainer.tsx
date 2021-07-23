ce7d61be6f1ca92ab8d2566dc59d5211d3fac49c
Bunyanut Suwankosit <amsteel23@gmail.com>
2021-07-23 15:59:14 +0700

    add SectionTitle in FormContainer

--- src/Pages/form/Containers/FormContainer.tsx
+++ src/Pages/form/Containers/FormContainer.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import ChiefComplaintSection from "../Components/ChiefComplaintSection";
 import NIHSSSection from "../Components/NIHSSSection";
 import SectionTitle from "../Components/SectionTitle";
+import UnderLyingDiseaseSection from "../Components/UnderLyingDiseaseSection";
 
 const FormContainer: React.FC = () => {
   return (
     <>
+      <SectionTitle title="Chief Complaint" />
+      <ChiefComplaintSection />
+      <SectionTitle title="Underlying Disease" />
+      <UnderLyingDiseaseSection />
       <SectionTitle title="NIHSS" />
       <NIHSSSection />
     </>
   );
 };
+
 export default FormContainer;
