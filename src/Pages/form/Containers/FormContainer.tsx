16e0d895fa86b3f63798bf24ab8cfda7069b63a8
palmmy2542 <palm_my2542@hotmail.com>
2021-07-23 17:44:32 +0700

    Add PatientInformationSection

--- src/Pages/form/Containers/FormContainer.tsx
+++ src/Pages/form/Containers/FormContainer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PatientInformationSection from "../Components/PatientInformationSection";
 import ChiefComplaintSection from "../Components/ChiefComplaintSection";
 import NIHSSSection from "../Components/NIHSSSection";
 import SectionTitle from "../Components/SectionTitle";
@@ -7,6 +8,8 @@ import UnderLyingDiseaseSection from "../Components/UnderLyingDiseaseSection";
 const FormContainer: React.FC = () => {
   return (
     <>
+      <SectionTitle title="Patient Information" />
+      <PatientInformationSection />
       <SectionTitle title="Chief Complaint" />
       <ChiefComplaintSection />
       <SectionTitle title="Underlying Disease" />
