351497d6997126ce3c92246fabd8b027c3522ed7
Bunyanut Suwankosit <amsteel23@gmail.com>
2021-07-23 17:15:44 +0700

    pull

--- src/Pages/Form/Containers/FormContainer.tsx
+++ src/Pages/Form/Containers/FormContainer.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import ChiefComplaintSection from "../Components/ChiefComplaintSection";
+import NIHSSSection from "../Components/NIHSSSection";
+import SectionTitle from "../Components/SectionTitle";
 import UnderLyingDiseaseSection from "../Components/UnderLyingDiseaseSection";
 
 const FormContainer: React.FC = () => {
   return (
     <>
+      <SectionTitle title="Chief Complaint" />
+      <ChiefComplaintSection />
+      <SectionTitle title="Underlying Disease" />
       <UnderLyingDiseaseSection />
+      <SectionTitle title="NIHSS" />
+      <NIHSSSection />
     </>
   );
 };
