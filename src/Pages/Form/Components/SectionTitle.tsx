9fc8fe1b752d5c17fc9f83e686265113ee42c016
Bunyanut Suwankosit <amsteel23@gmail.com>
2021-07-23 12:05:41 +0700

    Solve marginBottom in SectionTitle

--- src/Pages/Form/Components/SectionTitle.tsx
+++ src/Pages/Form/Components/SectionTitle.tsx
@@ -8,7 +8,7 @@ const useStyle = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       width: "100%",
-      padding: "50px",
+      marginBottom: "32px",
       alignItems: "center",
     },
     divider: {
