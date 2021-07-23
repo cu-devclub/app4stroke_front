7d7ac43ef68698a292b7054b71825a32713d64c5
palmmy2542 <palm_my2542@hotmail.com>
2021-07-23 17:56:46 +0700

    Remove unused import

--- src/Pages/form/Containers/FormContainer.tsx
+++ src/Pages/form/Containers/FormContainer.tsx
@@ -3,7 +3,6 @@ import PatientInformationSection from "../Components/PatientInformationSection";
 import ChiefComplaintSection from "../Components/ChiefComplaintSection";
 import NIHSSSection from "../Components/NIHSSSection";
 import SectionTitle from "../Components/SectionTitle";
-import ChiefComplaintSection from "../Components/ChiefComplaintSection";
 import EKG12LeadsSection from "../Components/EKG12LeadsSection";
 import UnderLyingDiseaseSection from "../Components/UnderLyingDiseaseSection";
 
