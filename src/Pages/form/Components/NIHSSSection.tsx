16ea2e352e7c9bf1f529c5a501db3ecdd23301b8
Bunyanut Suwankosit <amsteel23@gmail.com>
2021-07-26 15:10:48 +0700

    fixed commit Error

--- src/Pages/form/Components/NIHSSSection.tsx
+++ src/Pages/form/Components/NIHSSSection.tsx
@@ -35,59 +35,6 @@ const BestVisual = [
   { id: "CompleteHemianopia", title: "2 : Complete hemianopia" },
   { id: "BilateralHemianopia", title: "3 : Bilateral hemianopia" },
 ];
-
-const FacialPalsy = [
-  { id: "Normal", title: "0: Normal" },
-  { id: "Minor", title: "1: Minor" },
-  { id: "Partial", title: "2 : Partial" },
-  { id: "Complete", title: "3 : Complete" },
-];
-
-const BestMotor = [
-  { id: "NoDrift", title: "0 : No drift" },
-  { id: "Drift", title: "1 : Drift" },
-  { id: "FallIn10Seconds", title: "2 : Fall in 10 seconds" },
-  { id: "NoEffortAgainstGravity", title: "3 : No effort against gravity" },
-  { id: "NoMovement", title: "4 : No movement" },
-  { id: "AmputationOrJointFusion", title: "UN : Amputation or joint fusion" },
-];
-
-const LimbAtaxia = [
-  { id: "Absent", title: "0 : Absent" },
-  { id: "UpperOrLowerLimb", title: "1 : Upper or lower limb" },
-  { id: "UpperAndLowerLimb", title: "2 : Upper and lower limb" },
-  { id: "AmputationOrJointFusion", title: "UN : Amputation or joint fusion" },
-];
-
-const Sensory = [
-  { id: "Normal", title: "0 : Normal" },
-  { id: "PartialLoss", title: "1 : Partial loss" },
-  { id: "DenseLoss", title: "2 : Dense loss" },
-];
-
-const BestLanguageAphasia = [
-  { id: "No aphasia", title: "0 : No aphasia" },
-  { id: "MildToModerate", title: "1 : Mild to moderate" },
-  { id: "Severe", title: "2 : Severe" },
-  { id: "Mute", title: "3 : Mute" },
-];
-
-const Dysarthria = [
-  { id: "NormalArticulation", title: "0 : Normal articulation" },
-  { id: "MildToModerate", title: "1 : Mild to moderate" },
-  { id: "Severe", title: "2 : Severe" },
-  {
-    id: "IntubatedOrOtherPhysicalBarrier",
-    title: "UN : Intubated or other physical barrier",
-  },
-];
-
-const ExtinctionOrNeglect = [
-  { id: "NoNeglect", title: "0 : No neglect" },
-  { id: "SensoryOrVisual", title: "1 : Sensory or visual" },
-  { id: "SensoryAndVisual", title: "2 : Sensory and visual" },
-];
-
 const useStyles = makeStyles(() => ({
   textNIHSS: {
     marginBottom: "48px",
@@ -106,16 +53,6 @@ const RValues = {
   twoCommands: "",
   bestGaze: "",
   bestVisual: "",
-  facialPalsy: "",
-  bestMotorLeftArm: "",
-  bestMotorRightArm: "",
-  bestMotorLeftLeg: "",
-  bestMotorRightLeg: "",
-  limbAtaxia: "",
-  sensory: "",
-  bestLanguageAphasia: "",
-  dysarthria: "",
-  extinctionOrNeglect: "",
 };
 
 const NIHSSSection: React.FC = () => {
@@ -178,96 +115,6 @@ const NIHSSSection: React.FC = () => {
           onChange={handleInputChange}
           items={BestVisual}
         />
-      </Box>
-      <Box>
-        <Controls.PinkRadio
-          name="facialPalsy"
-          label="4. Facial palsy"
-          value={values.facialPalsy}
-          onChange={handleInputChange}
-          items={FacialPalsy}
-        />
-      </Box>
-      <Box>
-        <Controls.PinkRadio
-          name="bestMotorLeftArm"
-          label="5a. Best motor left arm"
-          value={values.bestMotorLeftArm}
-          onChange={handleInputChange}
-          items={BestMotor}
-        />
-        <Box>
-          <Controls.PinkRadio
-            name="bestMotorRightArm"
-            label="5b. Best motor right arm"
-            value={values.bestMotorRightArm}
-            onChange={handleInputChange}
-            items={BestMotor}
-          />
-        </Box>
-        <Box>
-          <Controls.PinkRadio
-            name="bestMotorLeftLeg"
-            label="6a. Best motor left leg"
-            value={values.bestMotorLeftLeg}
-            onChange={handleInputChange}
-            items={BestMotor}
-          />
-        </Box>
-        <Box>
-          <Controls.PinkRadio
-            name="bestMotorRightLeg"
-            label="6b. Best motor right leg"
-            value={values.bestMotorRightLeg}
-            onChange={handleInputChange}
-            items={BestMotor}
-          />
-        </Box>
-        <Box>
-          <Controls.PinkRadio
-            name="limbAtaxia"
-            label="7. Limb Ataxia"
-            value={values.limbAtaxia}
-            onChange={handleInputChange}
-            items={LimbAtaxia}
-          />
-        </Box>
-        <Box>
-          <Controls.PinkRadio
-            name="sensory"
-            label="8. Sensory"
-            value={values.sensory}
-            onChange={handleInputChange}
-            items={Sensory}
-          />
-        </Box>
-        <Box>
-          <Controls.PinkRadio
-            name="bestLanguageAphasia"
-            label="9. Best language Aphasia"
-            value={values.bestLanguageAphasia}
-            onChange={handleInputChange}
-            items={BestLanguageAphasia}
-          />
-        </Box>
-        <Box>
-          <Controls.PinkRadio
-            name="dysarthria"
-            label="10. Dysarthria"
-            value={values.dysarthria}
-            onChange={handleInputChange}
-            items={Dysarthria}
-          />
-        </Box>
-        <Box>
-          <Controls.PinkRadio
-            name="extinctionOrNeglect"
-            label="11. Extinction or neglect"
-            value={values.extinctionOrNeglect}
-            onChange={handleInputChange}
-            items={ExtinctionOrNeglect}
-          />
-        </Box>
       </Box>
     </>
   );
--- src/Pages/form/Containers/FormContainer.tsx
+++ src/Pages/form/Containers/FormContainer.tsx
@@ -3,9 +3,7 @@ import PatientInformationSection from "../Components/PatientInformationSection";
 import ChiefComplaintSection from "../Components/ChiefComplaintSection";
 import NIHSSSection from "../Components/NIHSSSection";
 import SectionTitle from "../Components/SectionTitle";
-import EKG12LeadsSection from "../Components/EKG12LeadsSection";
 import UnderLyingDiseaseSection from "../Components/UnderLyingDiseaseSection";
-import VitalSignsSection from "../Components/VitalSignsSection";
 
 const FormContainer: React.FC = () => {
   return (
@@ -16,10 +14,6 @@ const FormContainer: React.FC = () => {
       <ChiefComplaintSection />
       <SectionTitle title="Underlying Disease" />
       <UnderLyingDiseaseSection />
-      <SectionTitle title="Vital Signs" />
-      <VitalSignsSection />
-      <SectionTitle title="EKG 12 Leads" />
-      <EKG12LeadsSection />
       <SectionTitle title="NIHSS" />
       <NIHSSSection />
     </>
