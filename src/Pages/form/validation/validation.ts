e909dfa7deef35c55ac88a42b1d938732a4994aa
Wannakorn Cherngchaosil <bobbibrown.1502@gmail.com>
2021-07-29 16:40:54 +0700

    Create validate function

---
+++ src/Pages/form/validation/validation.ts
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+interface ValidateProps {
+  EKG12Leads: string;
+}
+const validate = (values: ValidateProps) => {
+  const errors = { EKG12Leads: "" };
+ 
+  if (
+    values.EKG12Leads === null ||
+    values.EKG12Leads === undefined ||
+    values.EKG12Leads === ""
+  ) {
+    errors.EKG12Leads = "Please select EKG 12 Leads";
+  }
+  return errors;
+};
+
+export default validate;
