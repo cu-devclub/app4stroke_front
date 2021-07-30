bf69ce1f52beae306c2816bed69665b254b10ad6
Wannakorn Cherngchaosil <bobbibrown.1502@gmail.com>
2021-07-30 13:02:45 +0700

    Create validate function

--- src/Pages/form/validation/validations.ts
+++ src/Pages/form/validation/validations.ts
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 interface ValidateProps {
   EKG12Leads: string;
 }
+
 const validate = (values: ValidateProps) => {
   const errors = { EKG12Leads: "" };
- 
+
   if (
     values.EKG12Leads === null ||
     values.EKG12Leads === undefined ||
     values.EKG12Leads === ""
-  ) {
+  ) {  
     errors.EKG12Leads = "Please select EKG 12 Leads";
   }
   return errors;
