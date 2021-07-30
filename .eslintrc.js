104eb1808261c8aa921536cac107cf778194fa86
palmmy2542 <palm_my2542@hotmail.com>
2021-07-30 15:34:32 +0700

    Edit eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,11 +15,8 @@ module.exports = {
     sourceType: "module",
   },
   rules: {
-    // we only want single quotes
     // we want to force semicolons
     semi: ["error", "always"],
-    // we use 2 spaces to indent our code
-    indent: ["error", 2],
     // we want to avoid useless spaces
     "no-multi-spaces": ["error"],
     "no-unused-vars": "off",
