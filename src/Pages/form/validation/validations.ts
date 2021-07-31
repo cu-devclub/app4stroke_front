6e137e11eab59d8e8a424771808e0c64b0a00065
Wannakorn Cherngchaosil <bobbibrown.1502@gmail.com>
2021-07-31 12:46:20 +0700

    Create validate function

--- src/Pages/form/validation/validations.ts
+++ src/Pages/form/validation/validations.ts
@@ -2,17 +2,25 @@ import React from "react";
 
 interface ValidateProps {
   EKG12Leads: string;
+  ChiefComplaint: string;
 }
 
 const validate = (values: ValidateProps) => {
-  const errors = { EKG12Leads: "" };
+  const errors = { EKG12Leads: "", ChiefComplaint: "" };
 
   if (
     values.EKG12Leads === null ||
     values.EKG12Leads === undefined ||
     values.EKG12Leads === ""
-  ) {  
+  ) {
     errors.EKG12Leads = "Please select EKG 12 leads";
+  } 
+  if (
+    values.ChiefComplaint === null ||
+    values.ChiefComplaint === undefined ||
+    values.ChiefComplaint === ""
+  ) {
+    errors.ChiefComplaint = "Please select time course";
   }
   return errors;
 };
