a8b8127b5faed3057941802e27eb30aa477dddf8
Wannakorn Cherngchaosil <bobbibrown.1502@gmail.com>
2021-07-31 12:53:12 +0700

    Create validate function

--- src/Pages/form/validation/validations.ts
+++ src/Pages/form/validation/validations.ts
@@ -1,20 +1,12 @@
 import React from "react";
 
 interface ValidateProps {
-  EKG12Leads: string;
   ChiefComplaint: string;
+  EKG12Leads: string;
 }
 
 const validate = (values: ValidateProps) => {
-  const errors = { EKG12Leads: "", ChiefComplaint: "" };
-
-  if (
-    values.EKG12Leads === null ||
-    values.EKG12Leads === undefined ||
-    values.EKG12Leads === ""
-  ) {
-    errors.EKG12Leads = "Please select EKG 12 leads";
-  } 
+  const errors = { ChiefComplaint: "",EKG12Leads: ""};
   if (
     values.ChiefComplaint === null ||
     values.ChiefComplaint === undefined ||
@@ -22,6 +14,13 @@ const validate = (values: ValidateProps) => {
   ) {
     errors.ChiefComplaint = "Please select time course";
   }
+  if (
+    values.EKG12Leads === null ||
+    values.EKG12Leads === undefined ||
+    values.EKG12Leads === ""
+  ) {
+    errors.EKG12Leads = "Please select EKG 12 leads";
+  } 
   return errors;
 };
 
