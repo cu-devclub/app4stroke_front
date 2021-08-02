2c958005e3f356bc0d75760bca7ee66ee954e042
pornthip-n <pawnthip@gmail.com>
2021-08-02 12:46:25 +0700

    remove unuse import

--- src/Pages/form/Components/Control/TimePicker.tsx
+++ src/Pages/form/Components/Control/TimePicker.tsx
@@ -1,19 +1,9 @@
 import "date-fns";
-import React, { useState } from "react";
-import { createStyles, makeStyles, withStyles } from "@material-ui/core/styles";
 import {
   MuiPickersUtilsProvider,
   KeyboardTimePicker,
-  KeyboardDatePicker,
 } from "@material-ui/pickers";
-import { MaterialUiPickersDate } from "@material-ui/pickers/typings/date";
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import ToggleButton from "@material-ui/lab/ToggleButton";
-import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
 import DateFnsUtils from "@date-io/date-fns";
-import intervalToDuration from "date-fns/intervalToDuration";
 
 export default function TimePicker(props: any) {
   const { label, onChange, value, className } = props;
