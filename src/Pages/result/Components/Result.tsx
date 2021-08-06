9191745effca098b5435c2cced28149a3a3930f7
Wannakorn Cherngchaosil <bobbibrown.1502@gmail.com>
2021-08-06 13:27:58 +0700

    Create result folder

---
+++ src/Pages/result/Components/Result.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+const Result: React.FC = () => {
+  return <>
+   
+  </>;
+};
+
+export default Result;
---
+++ src/Pages/result/Containers/ResultContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Result from "../Components/Result";
+
+const ResultContainer: React.FC = () => {
+  return (
+    <>
+      <Result />
+    </>
+  );
+};
+
+export default ResultContainer;
