33c64b71165a694fb9f83e8b271b9b9a42e64302
pornthip-n <pawnthip@gmail.com>
2021-08-20 15:58:22 +0700

    Change header style

--- src/Pages/HeaderFooter/Header.tsx
+++ src/Pages/HeaderFooter/Header.tsx
@@ -3,11 +3,10 @@ import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import strokeLogo from "../../Assets/stroke.png";
+import aiCuteLogo from "../../Assets/AICute.png";
 import PersonIcon from "@material-ui/icons/Person";
 import Avatar from "@material-ui/core/Avatar";
 
-//todo Style
 const useStyle = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -34,22 +33,21 @@ const useStyle = makeStyles((theme: Theme) =>
     icon: {
       margin: "auto",
       fontSize: "36px",
-      color: "#BEBFE2",
+      color: "#FFF3F9",
     },
     avatar: {
-      backgroundColor: "#505175",
+      backgroundColor: "#EF5DA8",
     },
   })
 );
 
-//todo Header
 const Header: React.FC = () => {
   const classes = useStyle();
   return (
     <div className={classes.root}>
       <AppBar position="static" color="inherit">
         <Toolbar className={classes.toolbar}>
-          <img src={strokeLogo} alt="icon" className={classes.logo} />
+          <img src={aiCuteLogo} alt="icon" className={classes.logo} />
           <Typography variant="h6" className={classes.name}>
             Dr. John Doe
           </Typography>
